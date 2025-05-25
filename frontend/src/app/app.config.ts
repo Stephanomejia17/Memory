5984184c59c96b716e0e967f3a7231658eba613d
Tepho <stephano.mejia@outlook.es>
2025-05-25 11:43:46 -0500

    merge

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideHttpClient(),]
-};
-=======
-=======
->>>>>>> d3fea90 (Avance)
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -22,24 +6,4 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
-<<<<<<< HEAD
->>>>>>> 9aae2f2 (Avance)
-=======
-=======
-=======
->>>>>>> b5365bf (Solicitar Servicio por usuario registrado)
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideHttpClient(),]
-};
-<<<<<<< HEAD
->>>>>>> 58fec67ef36fe7311f1c9fea10f520b2e620a7d8
->>>>>>> d3fea90 (Avance)
-=======
->>>>>>> b5365bf (Solicitar Servicio por usuario registrado)
