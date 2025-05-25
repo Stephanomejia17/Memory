299fce332e28bafcdb589cd7bc7c2f2d43ac783d
Melissa <70529928+foronmeli@users.noreply.github.com>
2025-05-25 00:09:27 -0500

    Update confirm.component.ts

--- frontend/src/app/pages/confirm/confirm.component.ts
+++ frontend/src/app/pages/confirm/confirm.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-confirm',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './confirm.component.html',
   styleUrl: './confirm.component.css'
 })
