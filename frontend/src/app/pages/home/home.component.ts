690f5763aa7c4fd699db130ba717ed203de18c4e
dhani-dev <dhani.dev22@gmail.com>
2025-05-26 23:24:13 -0500

    Diseño home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterLink } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PlansComponent } from '../plans/plans.component';
 import { ServiceComponent } from '../services-memory/services-memory.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, RouterLink, ReactiveFormsModule, PlansComponent, ServiceComponent],
+  imports: [CommonModule, HeaderComponent, RouterLink, ReactiveFormsModule, PlansComponent, ServiceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  backgroundImages: string[] = [
+    '/cielo1.jpg',
+    '/cielo5.jpg',
+    '/cielo3.jpg'
+  ];
+
+  currentIndex: number = 0;
+  private intervalId: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void {
+    this.stopCarousel();
+  }
+
+  private startCarousel(): void {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.backgroundImages.length;
+    }, 5000);
+  }
+
+  private stopCarousel(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
